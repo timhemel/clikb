f947919042aa00d054177e58da374ae541ad33c0
Tim Hemel <tim@securesoftware.nl>
2020-02-21 08:02:13 +0100

    Multi-line items in console output for 'show', with preliminary styling support

--- KanbanBoardRenderer.py
+++ KanbanBoardRenderer.py
@@ -21,6 +21,18 @@ class KanbanBoardBaseRenderer:
         pass
 
 
+class BoardView:
+    def __init__(self, num_columns):
+        self.num_columns = num_columns
+        self.clear()
+    def clear(self):
+        self.columns = [ [] for c in range(self.num_columns) ]
+    def add_field(self, column, lines):
+        self.columns[column] += lines
+    def get_rows(self):
+        rows = itertools.zip_longest(*self.columns)
+        return rows
+ 
 class KanbanBoardConsoleRenderer(KanbanBoardBaseRenderer):
 
     def __init__(self, app, field_fmt, out_file):
@@ -29,11 +41,13 @@ class KanbanBoardConsoleRenderer(KanbanBoardBaseRenderer):
         self.fieldfmt = field_fmt
         width, height = shutil.get_terminal_size() # TODO: ask click
         self.column_width = width // len(self.show_statuses)
+        self.board_view = BoardView(len(self.show_statuses))
 
     def _render_head(self):
-        row_text1 = " ".join([ self._render_head_field1(s) for s in self.show_statuses ])
-        row_text2 = " ".join([ self._render_head_field2(s) for s in self.show_statuses ])
-        return row_text1 + "\n" + row_text2 + "\n\n"
+        for i,s in enumerate(self.show_statuses):
+            self.board_view.add_field(i, [
+                self._render_head_field1(s),
+                self._render_head_field2(s) ])
 
     def _render_head_field1(self, s):
         t = "│ " + s + " " * self.column_width
@@ -43,26 +57,39 @@ class KanbanBoardConsoleRenderer(KanbanBoardBaseRenderer):
         t = "┕━" + "━" * (len(s)+1) + " " * self.column_width
         return t[:self.column_width - 1]
 
-    def _render_row(self, row):
-        return " ".join([self._render_field(f) for f in row])
-
-    def _render_field_content(self, field):
+    def _pad_and_truncate(self, text):
+        return (text + " " * (self.column_width-1))[:self.column_width-1] 
+    def _render_field(self, field):
         d = defaultdict( lambda : '?' )
         d.update(field)
-        return self.fieldfmt % d
+        return [ self._pad_and_truncate(f['text'] % d)
+                for f in self.fieldfmt ]
+        # fieldtext = "├╴" + self._render_field_content(field)
 
-    def _render_field(self, field):
-        if field is not None:
-            fieldtext = "├╴" + self._render_field_content(field)
-        else:
-            fieldtext = ""
-        fieldtext += " " * (self.column_width-1)
-        return fieldtext[:self.column_width-1]
+    def _render_item(self, item):
+        lines = self._render_field(item)
+        try:
+            col = self.column_lookup[item.get('status')]
+            self.board_view.add_field(col, lines)
+        except KeyError:
+            pass
 
     def render(self, rows):
-        lines = [ self._render_head() ]
-        lines += [ self._render_row(r) +"\n" for r in rows ]
-        self.outfile.write("".join(lines))
+        self.board_view.clear()
+        self.column_lookup = dict([ (v,i) for i,v in enumerate(self.show_statuses) ])
+        items = self.app.kanban_store.items()
+        self._render_head()
+        for i in items:
+            self._render_item(i)
+        rows = self.board_view.get_rows()
+        
+        for row in rows:
+            for f in row:
+                if f is not None:
+                    self.outfile.write(f)
+                else:
+                    self.outfile.write(' ' * self.column_width)
+            self.outfile.write('\n')
 
 
 class KanbanBoardCSVRenderer(KanbanBoardBaseRenderer):
